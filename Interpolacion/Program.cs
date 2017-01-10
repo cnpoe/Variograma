5b50352bdc38326f0b66b625c0dbefc1a7328426
cnpoe <cnpoek@gmail.com>
2017-01-10 16:10:30 -0600

    Test

--- Interpolacion/Program.cs
+++ Interpolacion/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
 
 namespace Interpolacion
 {
@@ -10,6 +11,8 @@ namespace Interpolacion
     {
         static void Main(string[] args)
         {
+            Herramientas.getData("ZoneA.dat");
+
         }
     }
 }
