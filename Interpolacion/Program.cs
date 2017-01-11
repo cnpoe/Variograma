b280413347dc8fd6950adca76ceb69110bfd818d
cnpoe <cnpoek@gmail.com>
2017-01-10 23:51:52 -0600

    sill

--- Interpolacion/Program.cs
+++ Interpolacion/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.Statistics;
 
 namespace Interpolacion
 {
@@ -11,18 +12,22 @@ namespace Interpolacion
     {
         static void Main(string[] args)
         {
-            List<List<double>> data = (List<List<double>>) Herramientas.getData("ZoneA.dat")["Data"];
+            List<List<double>> data = (List<List<double>>)Herramientas.getData("ZoneA.dat")["Data"];
             List<List<double>> P = new List<List<double>>();
 
             //List<int> pt = { 2000, 4700 };
             List<int> lags;
-             int tolerance = 250;
+            int tolerance = 250;
+            double sill;
 
             P.Add(data[0]);
             P.Add(data[1]);
             P.Add(data[3]);
 
-            lags = Herramientas.Arange(tolerance, 10000, tolerance*2);
+            lags = Herramientas.Arange(tolerance, 10000, tolerance * 2);
+
+            sill = Statistics.Variance(P[2]);
+
 
             System.Console.WriteLine("Hi");
         }
